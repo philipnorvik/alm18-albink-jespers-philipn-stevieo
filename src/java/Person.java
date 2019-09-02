c2a526e35da66eea86ab1603b55792ccb87229e0
albinkj91 <albinkj91@yahoo.com>
2019-09-02 17:50:10 +0200

    Person class done

--- src/java/Person.java
+++ src/java/Person.java
@@ -1,4 +1,31 @@
 
 public class Person {
     
+    private String name;
+    private int age;
+    private String nationality;
+    private String gender;
+
+    public Person(String name, int age, String nationality, String gender) {
+        this.name = name;
+        this.age = age;
+        this.nationality = nationality;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public String getGender() {
+        return gender;
+    }
 }
